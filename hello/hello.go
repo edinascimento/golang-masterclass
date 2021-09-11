31ebca0132a6bb709bf126aadce58e3aedccdedb
Édipo Nascimento <esnedipo@gmail.com>
2021-09-11 15:23:16 -0300

    Erro handle in Go

--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	// Get a greeting message and print it
-	message := greetings.Hello("Gladys")
+
+	// Set pproperties of the predefines Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number...
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	message, err := greetings.Hello("Gladys")
+	// If an error was retuned, print it to the console and
+	// exit the program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// If no erro was returned, print the returned message
+	// to the console
 	fmt.Println(message)
 }
